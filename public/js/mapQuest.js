446dfd74bc2d46e940544219d8c989334353dfeb
brendcrumb <bjew4b@gmail.com>
2020-02-27 11:13:20 -0800

    mapQuest

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = '8fc4FWtaaccV3wOJbHj4uQvgC7ttObIG';
+
+  var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapQuest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
